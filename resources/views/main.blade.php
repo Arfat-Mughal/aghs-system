16323771c8bc2bbea0941a2fab83cd7bb586f157
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2021-12-14 01:54:32 +0500

    datasheet added

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -53,6 +53,18 @@
 {{--                        </div>--}}
 {{--                    </div>--}}
                 </div>
+        <div class="slider-item" style="background-image:url({{asset('web_assets/images/datasheet.jpg')}});">
+{{--                    <div class="overlay"></div>--}}
+{{--                    <div class="container">--}}
+{{--                        <div class="row no-gutters slider-text align-items-center justify-content-center"--}}
+{{--                             data-scrollax-parent="true">--}}
+{{--                            <div class="col-md-8 text-center ftco-animate">--}}
+{{--                                <h1 class="mb-4">Need<span> For Your Child</span></h1>--}}
+{{--                                <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Read More</a></p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+                </div>
     </section>
 
     <section class="ftco-services ftco-no-pb">
