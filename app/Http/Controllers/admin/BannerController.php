999519400682d347a56b4ff9ff7ebf70c0fd014a
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2022-02-05 15:41:47 +0500

    Update BannerController.php

--- app/Http/Controllers/admin/BannerController.php
+++ app/Http/Controllers/admin/BannerController.php
@@ -31,7 +31,7 @@ class BannerController extends Controller
         $image_name = Str::random(25);
         $ext = strtolower($query->getClientOriginalExtension()); // You can use also getClientOriginalName()
         $image_full_name = $image_name . '.' . $ext;
-        $upload_path = 'banners/';    //Creating Sub directory in Public folder to put image
+        $upload_path = 'public/banners/';    //Creating Sub directory in Public folder to put image
         $image_url = $upload_path . $image_full_name;
         $query->move($upload_path, $image_full_name);
 
