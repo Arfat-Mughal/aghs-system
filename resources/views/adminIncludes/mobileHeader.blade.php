a9a7d643dad20f4066c81c46408b975c9554799a
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2024-01-19 18:54:25 +0500

    pending 1

--- resources/views/adminIncludes/mobileHeader.blade.php
+++ resources/views/adminIncludes/mobileHeader.blade.php
@@ -4,12 +4,13 @@
         <div class="container-fluid">
             <div class="header-mobile-inner">
                 <a class="logo" href="#">
-                    <img src="{{asset('admin_assets/images/logo_header.jpg')}}" alt="AGHS" width="80px" height="80px"/>
+                    <img src="{{ asset('admin_assets/images/logo_header.jpg') }}" alt="AGHS" width="80px"
+                        height="80px" />
                 </a>
                 <button class="hamburger hamburger--slider" type="button">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
                 </button>
             </div>
         </div>
@@ -18,36 +19,44 @@
         <div class="container-fluid">
             <ul class="navbar-mobile__list list-unstyled">
                 <li class="active has-sub">
-                    <a class="js-arrow" href="{{route('panel')}}">
+                    <a class="js-arrow" href="{{ route('panel') }}">
                         <i class="fas fa-tachometer-alt"></i>Dashboard</a>
                 </li>
                 <li>
-                    <a href="{{route('students')}}">
+                    <a href="{{ route('students') }}">
                         <i class="fas fa-copy"></i>Students</a>
                 </li>
                 <li>
-                    <a href="{{route('results')}}">
+                    <a href="{{ route('results') }}">
                         <i class="fas fa-chart-bar"></i>Results</a>
                 </li>
                 <li>
-                    <a href="{{route('slips')}}">
+                    <a href="{{ route('slips') }}">
                         <i class="fas fa-table"></i>Roll No Slip's</a>
                 </li>
                 <li>
-                    <a href="{{route('certificates')}}">
+                    <a href="{{ route('certificates') }}">
                         <i class="fas fa-trophy"></i>Short Courses</a>
                 </li>
                 <li>
-                    <a href="{{route('notifications')}}">
+                    <a href="{{ route('notifications') }}">
                         <i class="fas fa-flag"></i>Notifications</a>
                 </li>
                 <li>
-                    <a href="{{route('banners')}}">
+                    <a href="{{ route('banners') }}">
                         <i class="fas fa-sliders"></i>Banners</a>
                 </li>
                 <li>
-                    <a href="{{route('fees')}}">
+                    <a href="{{ route('fees') }}">
                         <i class="fas fa-calculator"></i>Fees</a>
+                </li>
+                <li>
+                    <a href="{{ route('grades.index') }}">
+                        <i class="fas fa-address-card"></i>Classes</a>
+                </li>
+                <li>
+                    <a href="{{ route('notes.index') }}">
+                        <i class="fas fa-linode"></i>Notes</a>
                 </li>
             </ul>
         </div>
