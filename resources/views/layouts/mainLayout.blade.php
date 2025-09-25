82c044d3cd40101870df134c4340e7b0b81c983e
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2025-09-25 21:08:56 +0500

    Update mainLayout.blade.php

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1672484806651208"
+         crossorigin="anonymous"></script>
 @include('includes.css')
 </head>
 <body>
