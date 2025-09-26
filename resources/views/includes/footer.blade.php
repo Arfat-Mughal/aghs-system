3a06a5b626a1b36eaa9fe1270aa888d7065df6fe
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2025-09-26 23:38:24 +0500

    improve the desgin

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,90 +1,63 @@
-<footer class="ftco-footer ftco-bg-dark ftco-section">
+<footer class="ftco-footer">
     <div class="container">
-        <div class="row mb-5">
+        <div class="row mb-4">
             <div class="col-md-6 col-lg-3">
-                <div class="ftco-footer-widget mb-5">
-                    <img src="{{asset('web_assets/images/logo_header.png')}}" alt="" width="90px" height="80px" class="mb-3">
-                    <h2 class="ftco-heading-2">AL-FALAH GRAMMAR HIGH SCHOOL & ACADEMY</h2>
+                <div class="ftco-footer-widget mb-4">
+                    <div style="background: white; padding: 10px; border-radius: 5px; display: inline-block; margin-bottom: 15px;">
+                        <img src="{{asset('web_assets/images/logo_header.png')}}" alt="" width="90px" height="80px">
+                    </div>
+                    <h2 class="ftco-heading-2 text-white">AL-FALAH GRAMMAR HIGH SCHOOL & ACADEMY</h2>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Contact Info</h2>
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2 text-white">Contact Info</h2>
                     <div class="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">VILLAGE BHANO CHAK P/O WAGHA TEHSIL SHALIMAR CANTT LAHORE</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">0321-4960275, 0321-4969849, 042-37172500</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@aghslahore.pk</span></a></li>
+                            <li><span class="icon icon-map-marker text-primary"></span><span class="text text-white">VILLAGE BHANO CHAK P/O WAGHA TEHSIL SHALIMAR CANTT LAHORE</span></li>
+                            <li><a href="tel:042-37172500"><span class="icon icon-phone text-primary"></span><span class="text text-white">0321-4960275, 0321-4969849, 042-37172500</span></a></li>
+                            <li><a href="mailto:info@aghslahore.pk"><span class="icon icon-envelope text-primary"></span><span class="text text-white">info@aghslahore.pk</span></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-{{--            <div class="col-md-6 col-lg-3">--}}
-{{--                <div class="ftco-footer-widget mb-5">--}}
-{{--                    <h2 class="ftco-heading-2">Recent Blog</h2>--}}
-{{--                    <div class="block-21 mb-4 d-flex">--}}
-{{--                        <a class="blog-img mr-4" style="background-image: url({{asset('web_assets/images/image_1.jpg')}});"></a>--}}
-{{--                        <div class="text">--}}
-{{--                            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>--}}
-{{--                            <div class="meta">--}}
-{{--                                <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>--}}
-{{--                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>--}}
-{{--                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="block-21 mb-5 d-flex">--}}
-{{--                        <a class="blog-img mr-4" style="background-image: url({{asset('web_assets/images/image_2.jpg')}});"></a>--}}
-{{--                        <div class="text">--}}
-{{--                            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>--}}
-{{--                            <div class="meta">--}}
-{{--                                <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>--}}
-{{--                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>--}}
-{{--                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
             <div class="col-md-6 col-lg-3">
-                <div class="ftco-footer-widget mb-5 ml-md-4">
-                    <h2 class="ftco-heading-2">Links</h2>
+                <div class="ftco-footer-widget mb-4 ml-md-4">
+                    <h2 class="ftco-heading-2 text-white">Quick Links</h2>
                     <ul class="list-unstyled">
-                        <li><a href="{{route('home')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
-                        <li><a href="{{route('roll_no')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Roll No Slip</a></li>
-                        <li><a href="{{route('result')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Result</a></li>
-{{--                        <li><a href="{{route('courses')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Courses</a></li>--}}
-                        <li><a href="{{route('contact')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact Us</a></li>
-                        <li><a href="{{route('about')}}"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+                        <li><a href="{{route('home')}}" class="text-white"><span class="ion-ios-arrow-round-forward mr-2 text-primary"></span>Home</a></li>
+                        <li><a href="{{route('roll_no')}}" class="text-white"><span class="ion-ios-arrow-round-forward mr-2 text-primary"></span>Roll No Slip</a></li>
+                        <li><a href="{{route('result')}}" class="text-white"><span class="ion-ios-arrow-round-forward mr-2 text-primary"></span>Result</a></li>
+                        <li><a href="{{route('contact')}}" class="text-white"><span class="ion-ios-arrow-round-forward mr-2 text-primary"></span>Contact Us</a></li>
+                        <li><a href="{{route('about')}}" class="text-white"><span class="ion-ios-arrow-round-forward mr-2 text-primary"></span>About</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Subscribe Us!</h2>
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2 text-white">Subscribe Us!</h2>
                     <form action="#" class="subscribe-form">
                         <div class="form-group">
-                            <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
-                            <input type="submit" value="Subscribe" class="form-control submit px-3">
+                            <input type="email" class="form-control mb-2 text-center" placeholder="Enter email address">
+                            <input type="submit" value="Subscribe" class="form-control submit px-3 btn-primary">
                         </div>
                     </form>
                 </div>
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2 mb-0 text-white">Connect With Us</h2>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
-                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#" class="text-white"><span class="icon-instagram"></span></a></li>
                     </ul>
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-md-12 text-center">
-
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                <p class="text-white mb-0">
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | AL-FALAH GRAMMAR HIGH SCHOOL & ACADEMY
+                </p>
             </div>
         </div>
     </div>
